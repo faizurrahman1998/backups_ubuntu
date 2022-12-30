f822959f51553fcc64cc72eb584c7b0901b7ec37
fayez <faizurrahman.fayez@gmail.com>
2022-12-30 12:53:19 +0600

    main: added alacritty

--- backup.sh
+++ backup.sh
@@ -9,4 +9,7 @@ cp /home/fayez/.vimrc /home/fayez/Public/Backups/.vimrc.bak
 echo "starship.toml"
 cp /home/fayez/.config/starship.toml /home/fayez/Public/Backups/starship.toml
 
+echo "alacritty.yml"
+cp /home/fayez/.config/alacritty.yml /home/fayez/Public/Backups/alacritty.yml.bak
+
 echo "completed"
